f41ab809c0a3eac88a41e89211fa0bf0de94d07d
thanhluan208 <skthunte33@gmail.com>
2024-11-13 23:14:57 +0700

    chore: resolve build err

--- src/Pages/Workflow/Components/CustomNodes/WF_LlmNode/Editor.tsx
+++ src/Pages/Workflow/Components/CustomNodes/WF_LlmNode/Editor.tsx
@@ -4,7 +4,7 @@ import {
   NodeOutPutVar,
 } from "@/Components/CommonStyles/EditorPlugin/type";
 import { Variable } from "@/Types/workflow";
-import { Box, useTheme } from "@mui/material";
+import { Box } from "@mui/material";
 import { Node, useReactFlow } from "@xyflow/react";
 import { useBoolean } from "ahooks";
 import { memo, useMemo } from "react";
@@ -24,7 +24,6 @@ const Editor = ({
   parentNodes,
   initValue,
 }: EditorProps) => {
-  const theme = useTheme();
   const { getNode } = useReactFlow();
   const onChange = (value: string) => {
     console.log(value);
