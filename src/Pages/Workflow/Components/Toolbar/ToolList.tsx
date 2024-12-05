26bf2b9a1d90fe3bb9e0a47a3a6e781f3e0ed85a
thanhluan208 <skthunte33@gmail.com>
2024-12-05 11:28:02 +0700

    feat: update tool list

--- src/Pages/Workflow/Components/Toolbar/ToolList.tsx
+++ src/Pages/Workflow/Components/Toolbar/ToolList.tsx
@@ -1,4 +1,3 @@
-import CommonIcons from "@/Components/CommonIcons";
 import CommonStyles from "@/Components/CommonStyles";
 import {
   Command,
@@ -7,10 +6,10 @@ import {
   CommandItem,
   CommandList,
 } from "@/Components/ui/command";
-import { useTheme } from "@mui/material";
+import { formatName } from "@/Helpers";
+import { Tooltip, useTheme } from "@mui/material";
 import { useCallback } from "react";
 import { Tool, ToolProvider } from "./type";
-import { capitalize } from "lodash";
 
 interface ToolListProps {
   toolProviders: ToolProvider[];
@@ -19,7 +18,11 @@ interface ToolListProps {
 const ToolList = ({ toolProviders }: ToolListProps) => {
   const theme = useTheme();
 
-  const onDragStart = (event: React.DragEvent<HTMLDivElement>, tool: Tool, providerName: string) => {
+  const onDragStart = (
+    event: React.DragEvent<HTMLDivElement>,
+    tool: Tool,
+    providerName: string
+  ) => {
     event.dataTransfer.setData(
       "application/reactflow",
       JSON.stringify({ nodeType: `tool.${providerName}.${tool.name}` })
@@ -44,7 +47,10 @@ const ToolList = ({ toolProviders }: ToolListProps) => {
 
         {toolProviders.map((provider) => {
           return (
-            <CommandGroup key={provider.provider} heading={capitalize(provider.provider)}>
+            <CommandGroup
+              key={provider.provider}
+              heading={formatName(provider.provider, "_")}
+            >
               {provider.tools.map((tool) => {
                 return (
                   <CommandItem
@@ -57,20 +63,27 @@ const ToolList = ({ toolProviders }: ToolListProps) => {
                   >
                     <div
                       className="px-3 w-full py-1 backdrop-blur cursor-grab border-none flex justify-between items-center rounded-md border "
-                      onDragStart={(event) => onDragStart(event, tool, provider.provider)}
+                      onDragStart={(event) =>
+                        onDragStart(event, tool, provider.provider)
+                      }
                       draggable
                     >
-                      <div className="flex gap-3 items-center">
-                        <img
-                          src={provider.identity.icon}
-                          alt="icon"
-                          className="rounded-lg w-6 h-6 object-cover object-center"
-                        />
-                        <CommonStyles.Typography type="semiBold16">
-                          {tool.name}
-                        </CommonStyles.Typography>
-                      </div>
-                      <CommonStyles.Button
+                      <Tooltip title={tool.name} placement="top-start">
+                        <div className="flex gap-3 items-center">
+                          <img
+                            src={provider.identity.icon}
+                            alt="icon"
+                            className="rounded-lg w-6 h-6 object-cover object-center"
+                          />
+                          <CommonStyles.Typography
+                            type="semiBold16"
+                            className="max-w-[200px] truncate"
+                          >
+                            {formatName(tool.name, "_")}
+                          </CommonStyles.Typography>
+                        </div>
+                      </Tooltip>
+                      {/* <CommonStyles.Button
                         variant="outlined"
                         startIcon={<CommonIcons.Add />}
                         sx={{
@@ -83,7 +96,7 @@ const ToolList = ({ toolProviders }: ToolListProps) => {
                         <CommonStyles.Typography type="semiBold16">
                           Add
                         </CommonStyles.Typography>
-                      </CommonStyles.Button>
+                      </CommonStyles.Button> */}
                     </div>
                   </CommandItem>
                 );
